7b6eb26fbc9a09431a7f6f201592097096f90304
lukaszgolojuch <lukasz.golojuch@gmail.com>
2022-06-14 21:36:14 +0200

    Initial Commit

---
+++ BMI Calculator/BMI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorApp.swift
+//  BMI Calculator
+//
+//  Created by Łukasz Gołojuch on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
